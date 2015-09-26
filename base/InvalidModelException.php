2b70aeab2fbedf0a5421eda94c8791093482ec00
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-26 07:22:17 +0300

    add __construct()

--- base/InvalidModelException.php
+++ base/InvalidModelException.php
@@ -2,14 +2,35 @@
 
 namespace yii\boost\db;
 
-use UnexpectedValueException;
+use Exception,
+    yii\base\Model,
+    UnexpectedValueException;
 
 
 class InvalidModelException extends UnexpectedValueException
 {
 
+    /**
+     * @var Model
+     */
+    private $_model = null;
+
+    public function __construct(Model $model, $message = null, $code = 0, Exception $previous = null)
+    {
+        $this->_model = $model;
+        parent::__construct($message, $code, $previous);
+    }
+
     public function getName()
     {
         return 'Invalid Model';
+    }
+
+    /**
+     * @return Model
+     */
+    public function getModel()
+    {
+        return $this->_model;
     }
 }
