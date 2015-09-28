dd336ac60bb96eed81a41b1982687f0e1e6c6e73
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-28 06:16:28 +0300

    add ReadOnlyActiveRecord

---
+++ db/ReadOnlyActiveRecord.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace yii\boost\db;
+
+use yii\base\NotSupportedException;
+
+
+class ReadOnlyActiveRecord extends ActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function updateAll($attributes, $condition = '', $params = [])
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function updateAllCounters($counters, $condition = '', $params = [])
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function deleteAll($condition = '', $params = [])
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function save($runValidation = true, $attributeNames = null)
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function insert($runValidation = true, $attributes = null)
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function insertInternal($attributes = null)
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function update($runValidation = true, $attributeNames = null)
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function updateInternal($attributes = null)
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeSave($insert)
+    {
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function delete()
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function deleteInternal()
+    {
+        throw new NotSupportedException(__METHOD__ . ' is not supported.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeDelete()
+    {
+        return false;
+    }
+}
