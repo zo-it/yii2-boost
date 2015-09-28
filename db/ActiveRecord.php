cdfffbba6a4e6db1e04175e11cb7a5b17944e062
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-09-28 06:41:32 +0300

    add dumpData()

--- db/ActiveRecord.php
+++ db/ActiveRecord.php
@@ -21,73 +21,44 @@ class ActiveRecord extends YiiActiveRecord
         return Yii::createObject(ActiveQuery::className(), [get_called_class()]);
     }
 
-    public function log($message = 'dump', $category = 'application')
+    public function dumpData()
     {
         if ($this->hasErrors()) {
-            $data = [
+            return [
                 'class' => get_class($this),
                 'attributes' => $this->getAttributes(),
                 'errors' => $this->getErrors()
             ];
-            Yii::error($message . PHP_EOL . VarDumper::dumpAsString($data), $category);
         } else {
-            $data = [
+            return [
                 'class' => get_class($this),
                 'attributes' => $this->getAttributes()
             ];
-            Yii::info($message . PHP_EOL . VarDumper::dumpAsString($data), $category);
+        }
+    }
+
+    public function log($message = 'dump', $category = 'application')
+    {
+        if ($this->hasErrors()) {
+            Yii::error($message . PHP_EOL . VarDumper::dumpAsString($this->dumpData()), $category);
+        } else {
+            Yii::info($message . PHP_EOL . VarDumper::dumpAsString($this->dumpData()), $category);
         }
     }
 
     public function fb($label = null)
     {
-        if ($this->hasErrors()) {
-            $data = [
-                'class' => get_class($this),
-                'attributes' => $this->getAttributes(),
-                'errors' => $this->getErrors()
-            ];
-        } else {
-            $data = [
-                'class' => get_class($this),
-                'attributes' => $this->getAttributes()
-            ];
-        }
-        FB::log($data, $label);
+        FB::log($this->dumpData(), $label);
     }
 
     public function dump()
     {
-        if ($this->hasErrors()) {
-            $data = [
-                'class' => get_class($this),
-                'attributes' => $this->getAttributes(),
-                'errors' => $this->getErrors()
-            ];
-        } else {
-            $data = [
-                'class' => get_class($this),
-                'attributes' => $this->getAttributes()
-            ];
-        }
-        VarDumper::dump($data);
+        VarDumper::dump($this->dumpData());
     }
 
     public function dumpAsString()
     {
-        if ($this->hasErrors()) {
-            $data = [
-                'class' => get_class($this),
-                'attributes' => $this->getAttributes(),
-                'errors' => $this->getErrors()
-            ];
-        } else {
-            $data = [
-                'class' => get_class($this),
-                'attributes' => $this->getAttributes()
-            ];
-        }
-        return VarDumper::dumpAsString($data);
+        return VarDumper::dumpAsString($this->dumpData());
     }
 
     /**
